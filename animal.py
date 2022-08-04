356a2f4cf62885b4c4a59f30376ac1b28a0d8d29
Christian <Moore.Christian76@gmail.com>
2022-08-04 12:17:33 +0100

    added reptile.py

--- animal.py
+++ animal.py
@@ -1,19 +1,22 @@
-class animal:
+class Animal:
 
-    def __init__(self):
-        species_name = "animal"
-        movement_type = "moves"
+    # __init__ is a constructor which declares class attributes
+    def __init__(self): # self refers to current class
+        self.species_name = "animal"
+        self.movement_type = "moves"
+        self.alive = True
+        self.spine = True
 
-    def sleep(self):
-        print(self.species_name, "is sleeping....")
+    def sleep(self): # returns concatenated string of animal sleeping
+        return "The", self.species_name, "is sleeping...."
 
-    def eat(self):
-        print(self.species_name, "is eating")
+    def eat(self): # returns concatenated string of animal eating
+        return "The", self.species_name, "is eating"
 
-    def move(self):
-        print(self.species_name, self.movement_type)
+    def move(self): # returns concatenated string of animal moving
+        return "The", self.species_name, self.movement_type
 
+# create an object of the class
+cat = Animal()
 
-this_animal = animal
-
-this_animal.move()
+#print(cat.move())
--- reptile.py
+++ reptile.py
@@ -1,37 +1,3 @@
-# python OOP
-
-![OOP diagram](https://user-images.githubusercontent.com/47668244/182827918-e4e87130-63ea-4814-899a-ff8eb70053a5.png)
-
-## Animal
-
-```commandline
-class Animal:
-
-    # __init__ is a constructor which declares class attributes
-    def __init__(self): # self refers to current class
-        self.species_name = "animal"
-        self.movement_type = "moves"
-        self.alive = True
-        self.spine = True
-
-    def sleep(self): # returns concatenated string of animal sleeping
-        return "The", self.species_name, "is sleeping...."
-
-    def eat(self): # returns concatenated string of animal eating
-        return "The", self.species_name, "is eating"
-
-    def move(self): # returns concatenated string of animal moving
-        return "The", self.species_name, self.movement_type
-
-# create an object of the class
-cat = Animal()
-
-#print(cat.move())
-
-```
-## Reptile
-
-```commandline
 # create a reptile class which inherit everything from animal class into reptile
 from animal import Animal
 
@@ -59,5 +25,3 @@ a_reptile = Reptile()
 # prints the behaviour methods of the reptile & from the inherited animal through reptile
 print(a_reptile.seek_heat())
 print(a_reptile.move())
-
-```
